a1025a20a1c21868a041721ae2c4dd8fd1b61764
linhQuag <qlinh280502@gmail.com>
2023-07-23 12:42:00 +0700

    add comment

--- array7_1.cpp
+++ array7_1.cpp
@@ -9,8 +9,11 @@ int main(){
     for(int i =0; i<n; i++){
         cin >> a[i];
     }
+
     int r;
     int b[a[n-1]][2];
+
+    // sắp xếp lại mảng
     for(int i = 0; i<n; i++){
         for(int j = i + 1; j<n;j++){
             if(a[i] > a[j]){
@@ -20,8 +23,24 @@ int main(){
             }
         }
     }
+
+    // biến count để đếm số lần xuất hiện
+    // biến k để lưu vị trí của giá trị a[i] và count
     int count, k;
     k = 0;
+    
+    /*
+    dãy: 2 2 4 4 5 6 6
+    i = 0, a[0] = 2, count = 2 --> bước nhảy = 0 + 2
+    i = 2 , a[2] = 4, count = 2 --> bước nhảy = 2 + 2
+    i = 4, a[4] = 5, count = 1 --> bước nhảy = 4 + 1
+    i = 5, a[5] = 5, count = 2 --> bước nhảy = 5 + 2 --> thoát khỏi vòng lặp
+
+    b có dạng: 
+        0 1 2 3 
+    0   2 2 1 2
+    1   2 4 5 6 
+    */
     for(int i = 0; i< n; i = count + i){
         count = 1;
         for(int j = i+1; j<n; j++){
