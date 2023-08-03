9df259e1f3bebf7b5f90e1f1249f1d65bc823b4e
linhQuag <qlinh280502@gmail.com>
2023-08-03 10:54:02 +0700

    upload arr10

---
+++ array10.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int m, n;
+    cin >> m >> n;
+    int a[m][n];
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j<n; j++){
+            cin >> a[i][j];
+        }
+    }
+
+    int sum = 0;
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j<n; j++){
+            sum += a[i][j];
+        }
+    }
+
+    cout << sum;
+    
+    return 0;
+}
